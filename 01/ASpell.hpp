2501a40674bc3ec68bb3498faa503a8115ecd7d9
Khussein Reber <73395161+hmanilow@users.noreply.github.com>
2023-01-21 21:20:12 +0300

    CAsp

---
+++ 01/ASpell.hpp
@@ -0,0 +1,30 @@
+
+#pragma once 
+#include <iostream>
+#include "ATarget.hpp"
+class ATarget;
+
+class ASpell {
+public:
+    ASpell(std::string name, std::string effects) {
+        _name = name;
+        _effects = effects;    
+    }
+    virtual ~ASpell() {} //для удаления объектов производных классов
+
+    std::string getName()
+    {
+        return _name;
+    }
+    std::string getEffects() const
+    {
+        return _effects;
+    }
+
+    void launch(const ATarget &target);
+    virtual ASpell* clone() const = 0;
+
+private:
+    std::string _name;
+    std::string _effects;
+};
