d68b145c9cb27b9f51beaa9b91785cbd2227ea54
Khussein Reber <73395161+hmanilow@users.noreply.github.com>
2023-01-26 18:04:27 +0300

    PolyHoly

---
+++ 02/Polymorph.hpp
@@ -0,0 +1,12 @@
+
+#pragma once
+#include "ASpell.hpp"
+
+class Polymorph : public ASpell {
+public:
+    Polymorph() : ASpell("Polymorph", "turned into a critter") {}
+    ~Polymorph() {}
+    virtual ASpell* clone() const {
+        return new Polymorph();
+    }
+};
